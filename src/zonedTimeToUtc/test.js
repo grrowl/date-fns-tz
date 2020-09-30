b381d81c59634291882044ced1aa6866b52ab5c1
Tom McKenzie <tom@chillidonut.com>
2020-09-30 15:40:18 +1000

    Add zonedTimeToUtc test for AEST to AEDT daylight savings changeover

--- src/zonedTimeToUtc/test.js
+++ src/zonedTimeToUtc/test.js
@@ -45,4 +45,33 @@ describe('zonedTimeToUtc', function() {
     var result = zonedTimeToUtc(new Date(2014, 5, 25, 10, 0, 0, 123), 'Z')
     assert.deepEqual(result.toISOString(), '2014-06-25T10:00:00.123Z')
   })
+
+  describe('near DST changeover (AEST to AEDT)', function() {
+    it('zoned time one day before', function() {
+      var result = zonedTimeToUtc(
+        new Date('2020-10-03T17:00:00.000'),
+        'Australia/Melbourne' // +10 hours
+      )
+
+      assert.deepEqual(result.toISOString(), '2020-10-03T07:00:00.000Z')
+    })
+
+    it('zoned time 15 minutes before', function() {
+      var result = zonedTimeToUtc(
+        new Date('2020-10-04T01:45:00.000'),
+        'Australia/Melbourne' // +10 hours
+      )
+
+      assert.deepEqual(result.toISOString(), '2020-10-03T15:45:00.000Z')
+    })
+
+    it('zoned time 15 minutes after', function() {
+      var result = zonedTimeToUtc(
+        new Date('2020-10-04T02:15:00.000'),
+        'Australia/Melbourne' // +11 hours
+      )
+
+      assert.deepEqual(result.toISOString(), '2020-10-03T16:15:00.000Z')
+    })
+  })
 })
