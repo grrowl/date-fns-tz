1ba2c78461e9180affb0932dab5723b3af73252a
Tom McKenzie <tom@chillidonut.com>
2020-10-02 00:06:12 +1000

    Just tests

--- src/toDate/index.js
+++ src/toDate/index.js
@@ -156,7 +156,7 @@ export default function toDate(argument, dirtyOptions) {
     if (dateStrings.timezone || options.timeZone) {
       offset = tzParseTimezone(
         dateStrings.timezone || options.timeZone,
-        date
+        new Date(timestamp + time)
       )
       if (isNaN(offset)) {
         return new Date(NaN)
